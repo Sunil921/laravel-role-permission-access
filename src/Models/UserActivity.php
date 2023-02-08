5ecf08674661555d0a81e3354f9fe098fc5e2ae8
Sunil Thakur <sunilthakur178co@gmail.com>
2023-02-08 11:13:37 +0530

    Added user and module relationship in user activity

--- src/Models/UserActivity.php
+++ src/Models/UserActivity.php
@@ -6,12 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use Sunil\LaravelRolePermissionAccess\Traits\TableInfoTrait;
+use App\Models\User;
 
 class UserActivity extends Model
 {
     use HasFactory, SoftDeletes, TableInfoTrait;
 
-    protected $casts = [
-        'body' => 'array',
-    ];    
+    // protected $casts = [ 'body' => 'array' ];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'added_by');
+    }
+
+    public function module()
+    {
+        return $this->belongsTo(Module::class);
+    }
 }
